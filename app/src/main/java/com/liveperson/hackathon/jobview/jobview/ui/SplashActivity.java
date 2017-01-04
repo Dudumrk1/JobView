9661f116e82104fa9b31337ae5ad28f191dd6af8
dudu <dudu@liveperson.com>
2017-01-04 15:07:05 +0200

    Splash screen

---
+++ app/src/main/java/com/liveperson/hackathon/jobview/jobview/ui/SplashActivity.java
@@ -0,0 +1,98 @@
+package com.liveperson.hackathon.jobview.jobview.ui;
+
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ui.ResultCodes;
+import com.google.firebase.auth.FirebaseAuth;
+import com.liveperson.hackathon.jobview.jobview.R;
+import com.liveperson.hackathon.jobview.jobview.utils.PreferenceManager;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 12345;
+    private static final String IS_FIRST_LOGIN = "is_first_login";
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 2000;
+
+    View mRootView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mRootView = findViewById(R.id.splash_layout);
+
+        PreferenceManager.initializeInstance(getApplicationContext());
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() == null) {
+            // not signed in
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .build(),
+                    RC_SIGN_IN);
+            finish();
+        } else {
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i = new Intent(SplashActivity.this, DashboardActivity.class);
+                    startActivity(i);
+
+                    // close this activity
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        }
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // user is signed in!
+                Snackbar.make(mRootView, "Log-in ended successfully", Snackbar.LENGTH_LONG).show();
+
+                //Check if need to fill the personal details
+                boolean firstLogin = PreferenceManager.getInstance().getBooleanValue(IS_FIRST_LOGIN, true);
+                if (firstLogin){
+                    startActivity(new Intent(this, PersonalDetailsActivity.class));
+                    finish();
+
+                }
+                return;
+            }
+
+            // Sign in canceled
+            if (resultCode == RESULT_CANCELED) {
+                Snackbar.make(mRootView, "Log-in was canceled", Snackbar.LENGTH_LONG).show();
+                return;
+            }
+
+            // No network
+            if (resultCode == ResultCodes.RESULT_NO_NETWORK) {
+                Snackbar.make(mRootView, "Log-in failed - No Network", Snackbar.LENGTH_LONG).show();
+                return;
+            }
+
+            // User is not signed in. Maybe just wait for the user to press
+            // "sign in" again, or show a message.
+        }
+    }
+
+}
