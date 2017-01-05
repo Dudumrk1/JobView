7d5d9e87324b96627084ec860e0b531b5c1fc655
asherh <asherh@liveperson.com>
2017-01-05 08:31:41 +0200

    working on downloadVideo

--- app/src/main/java/com/liveperson/hackathon/jobview/jobview/ui/VideoRecordingActivity.java
+++ app/src/main/java/com/liveperson/hackathon/jobview/jobview/ui/VideoRecordingActivity.java
@@ -3,6 +3,7 @@ package com.liveperson.hackathon.jobview.jobview.ui;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -15,12 +16,14 @@ import android.widget.VideoView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.liveperson.hackathon.jobview.jobview.R;
 
 import java.io.File;
+import java.io.IOException;
 
 public class VideoRecordingActivity extends BaseDrawerActivity {
 
@@ -89,16 +92,57 @@ public class VideoRecordingActivity extends BaseDrawerActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
             Uri videoUri = intent.getData();
-            String fileName = videoUri.getPath();
+            String realPath = getRealPathFromURI(getApplicationContext(), videoUri);
 //            Toast toast = Toast.makeText(getApplicationContext(), intent.getDataString(), Toast.LENGTH_LONG);
 //            toast.show();
 //            mVideoView.setVideoURI(videoUri);
 
 
-                uploadVideo(fileName);
+                uploadVideo(realPath);
 
 //            com.liveperson.hackathon.jobview.jobview.controller.SessionManager.getInstance().
         }
+
+    }
+
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    private void downloadVideo(String pathToLook){
+        StorageReference islandRef = mStorageRef.child(pathToLook);
+        mProgress = new ProgressDialog(this);
+        mProgress.setMessage("Downloading Video...");
+        mProgress.show();
+        File localFile = null;
+        try {
+            localFile = File.createTempFile("videos_" + System.currentTimeMillis(), "mp4");
+            islandRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    mProgress.dismiss();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    mProgress.dismiss();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
@@ -107,7 +151,7 @@ public class VideoRecordingActivity extends BaseDrawerActivity {
         mProgress.setMessage("Uploading Video...");
         mProgress.show();
         Uri file = Uri.fromFile(new File(fileName));
-        StorageReference riversRef = mStorageRef.child("newVideo.mp4");
+        StorageReference riversRef = mStorageRef.child("newVideo_" + System.currentTimeMillis() + ".mp4");
         riversRef.putFile(file).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>(){
             @Override
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot){
