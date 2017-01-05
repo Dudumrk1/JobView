f7cbe00b1002077362324c4c35901d41349837af
asherh <asherh@liveperson.com>
2017-01-05 10:34:47 +0200

    AboutActivity

---
+++ app/src/main/java/com/liveperson/hackathon/jobview/jobview/ui/AboutActivity.java
@@ -0,0 +1,35 @@
+package com.liveperson.hackathon.jobview.jobview.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.liveperson.hackathon.jobview.jobview.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        String data = getResources().getString(R.string.AboutStr);
+        TextView AboutText = (TextView) findViewById(R.id.AboutTxt);
+        AboutText.setText(data);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
